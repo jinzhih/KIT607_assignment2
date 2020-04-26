aea1817c56a951f3616e1c77e287f05cc5a09462
Jessie Hou <jessie.houjinzhi@gmail.com>
2020-04-26 16:35:10 +1000

    new a project

---
+++ KIT607_Raffle/KIT607_Raffle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KIT607_Raffle
+//
+//  Created by Jin Hou on 26/4/20.
+//  Copyright © 2020 Jinzhi Hou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
