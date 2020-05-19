9a3035a72ee9570db58990702898bf9a07173c5f
Jessie Hou <jessie.houjinzhi@gmail.com>
2020-05-20 03:06:55 +1000

    update searchBar

--- KIT607_Raffle/KIT607_Raffle/CustomerCusTableViewController.swift
+++ KIT607_Raffle/KIT607_Raffle/CustomerCusTableViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 var customers1 = [Customer]()
 
 class CustomerCusTableViewController: UIViewController {
-    
+     var searchResult = [Customer]()
      var raffleTemp : Raffle?
-
+    var searching = false
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet weak var customerTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +34,7 @@ class CustomerCusTableViewController: UIViewController {
       alert.addAction(UIAlertAction(title: "Add Customer", style: .default, handler: { (action: UIAlertAction!) in
           // insert customer into database
            var database : SQLiteDatabase = SQLiteDatabase(databaseName:"MyDatabase")
+        
         database.insert(customer: Customer(customerName:textField.text!))
         }))
 
@@ -40,24 +43,75 @@ class CustomerCusTableViewController: UIViewController {
     }
     
 }
+//TOdosearch Bar
+extension CustomerCusTableViewController:UISearchBarDelegate{
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//         var database : SQLiteDatabase = SQLiteDatabase(databaseName:"MyDatabase")
+//       //let ids = array.map { $0.id }
+//        let customerList = customers1.map{$0.customerName}
+//        searchResult = database.selectCustomerByName(customerName: searchText)
+//        print("hello")
+//        searching = true
+//        customerTable.reloadData()
+//    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+         var database : SQLiteDatabase = SQLiteDatabase(databaseName:"MyDatabase")
+        print(searchBar.text!)
+        searchResult = database.selectCustomerByName(customerName: searchBar.text!)
+        print("hello")
+        searching = true
+        customerTable.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            searching = false
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
 
-extension CustomerCusTableViewController: UITableViewDataSource{
+
+extension CustomerCusTableViewController: UITableViewDataSource, UITableViewDelegate{
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return customers1.count
+    if searching{
+        return searchResult.count
+    }else{
+        return customers1.count
+    }
+    
 }
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    let cell = tableView.dequeueReusableCell(withIdentifier: "CustomerCell", for: indexPath)
-    
-    // Configure the cell...
-    let customer = customers1[indexPath.row]
-    if   let  customerCell = cell as? CustomerTableViewCell
-    {
-        customerCell.customerID.text = String(customer.ID)
-        customerCell.customerName.text = customer.customerName
+var cell = tableView.dequeueReusableCell(withIdentifier: "CustomerCell", for: indexPath)
+    if searching {
         
+    var customer = searchResult[indexPath.row]
+        if   let  customerCell = cell as? CustomerTableViewCell
+        {
+            customerCell.customerID.text = String(customer.ID)
+            customerCell.customerName.text = customer.customerName
+            
+            
+        }
+    } else{
         
+        // Configure the cell...
+        let customer = customers1[indexPath.row]
+        if   let  customerCell = cell as? CustomerTableViewCell
+        {
+            customerCell.customerID.text = String(customer.ID)
+            customerCell.customerName.text = customer.customerName
+            
+            
+        }
+
     }
+    
+    
+    
     return cell
    
     
